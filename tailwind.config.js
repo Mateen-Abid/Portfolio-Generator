126ed708fda4543959121a4fc430d26746fd916e
Mateen Abid <138778244+Mateen-Abid@users.noreply.github.com>
2025-03-23 19:47:49 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // ✅ Enable dark mode with class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
